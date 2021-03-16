0297569e22b566b5c590769dc100acb2b517a9eb
Jitin Nair <jitin.s.nair@gmail.com>
2021-03-16 16:34:20 +0530

    added vim plugins

--- link.sh
+++ link.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
-ln -sf $SCRIPT_DIR/.zshenv ~/.zshenv
-ln -sf $SCRIPT_DIR/.zshrc ~/.zshrc 
+ln -sf $SCRIPT_DIR/.vim/plugins.vim ~/plugins.vim
+ln -sf $SCRIPT_DIR/.zshrc ~/.zshrc
+ln -sf $SCRIPT_DIR/.leptonrc ~/.leptonrc
+ln -sf $SCRIPT_DIR/.p10k.zsh ~/.p10k.zsh
+
---
+++ plugins.vim
@@ -0,0 +1,12 @@
+" Specify a directory for plugins
+call plug#begin('~/.vim/plugged')
+
+" Shorthand notation for plugin
+Plug 'itchyny/lightline.vim'
+Plug 'junegunn/fzf'
+Plug 'junegunn/fzf.vim'
+Plug 'scrooloose/nerdtree'
+Plug 'tpope/vim-surround'
+
+" Initialize plugin system
+call plug#end()
