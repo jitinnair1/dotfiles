213efe056fcdd7ff9caa3908b4319aaca2d5c9c5
JItin Nair <jitin.s.nair@gmail.com>
2021-11-20 14:25:26 +0530

    fixed vimrc link

--- link.sh
+++ link.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
-ln -sf $SCRIPT_DIR/.vim/plugins.vim ~/plugins.vim
+ln -sf $SCRIPT_DIR/plugins.vim ~/.vim/plugins.vim
 ln -sf $SCRIPT_DIR/.zshrc ~/.zshrc
 ln -sf $SCRIPT_DIR/.leptonrc ~/.leptonrc
 ln -sf $SCRIPT_DIR/.p10k.zsh ~/.p10k.zsh
-ln -sf $SCRIPT_DIR/.p10k.zsh ~/.p10k.zsh
 sudo ln -sf $SCRIPT_DIR/functions/vtk2png.py /usr/local/bin/vtk2png.py
-
