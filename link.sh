52aaa74282f3bd0eae48136d3c2437441850e808
Jitin Nair <jitins@iisc.ac.in>
2022-02-12 11:14:03 +0530

    added vtk2pngTern

--- link.sh
+++ link.sh
@@ -6,3 +6,4 @@ ln -sf $SCRIPT_DIR/.zshrc ~/.zshrc
 ln -sf $SCRIPT_DIR/.leptonrc ~/.leptonrc
 ln -sf $SCRIPT_DIR/.p10k.zsh ~/.p10k.zsh
 sudo ln -sf $SCRIPT_DIR/functions/vtk2png.py /usr/local/bin/vtk2png.py
+sudo ln -sf $SCRIPT_DIR/functions/vtk2pngTern.py /usr/local/bin/vtk2pngTern.py
