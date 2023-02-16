d526a389f28d3d7312909ea3ab0f72ac9dc8c6de
Jitin Nair <jitin.s.nair@gmail.com>
2023-02-17 02:55:19 +0530

    removed TODO

--- link.sh
+++ link.sh
@@ -61,7 +61,3 @@ ln -sf $SCRIPT_DIR/.p10k.zsh ~/.p10k.zsh
 #sudo ln -sf $SCRIPT_DIR/functions/vtk2pngTernThreePhase.py /usr/local/bin/vtk2pngTernThreePhase.py
 #sudo ln -sf $SCRIPT_DIR/functions/vtk2pngTernTwoPhaseClipped.py /usr/local/bin/vtk2pngTernTwoPhaseClipped.py
 #sudo ln -sf $SCRIPT_DIR/functions/vtk2pngTernThreePhaseClipped.py /usr/local/bin/vtk2pngTernThreePhaseClipped.py
-
-#TODO
-# 1. Create a seperate install.sh for new decive setup
-# 2. Clean up functions in zshrc and custom functions here
