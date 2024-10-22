ffabe3b94517dc5d2bdeef6778302318c981eb09
Jitin Nair <jitin.s.nair@gmail.com>
2024-10-22 18:47:19 +0530

    added nvim config

---
+++ config/nvim/lua/vim-options.lua
@@ -0,0 +1,26 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- From my vimrc
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set noshowmode")
+vim.cmd("set noshowcmd")
+vim.cmd("set shortmess+=F")
+
+-- From cpow's template
+vim.g.mapleader = " "
+vim.g.background = "light"
+
+vim.opt.swapfile = false
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
