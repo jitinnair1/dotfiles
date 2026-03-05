add68b36a6101f3bdd2bc2dbff468947d1251c2c
Jitin <jitin.s.nair@gmail.com>
2026-03-05 12:19:11 +0000

    cleaned up scripts

--- stow.sh
+++ stow.sh
@@ -8,19 +8,12 @@ cd "$DOTFILES_DIR"
 echo "Installing dotfiles from $DOTFILES_DIR to $TARGET_DIR"
 
 #stow
-stow --target="$TARGET_DIR" --verbose shell nvim tmux alacritty ghostty zed others vim i3
+stow --target="$TARGET_DIR" --verbose shell nvim tmux alacritty ghostty zed others vim opencode
 
 #tpm
 if [ ! -d ~/.tmux/plugins/tpm ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     echo "tpm installed"
-fi
-
-#iTerm preferences (if on macOS)
-if [[ "$OSTYPE" == "darwin"* ]] && [ -d '/Applications/iTerm.app' ]; then
-    defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$DOTFILES_DIR/config/"
-    defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
-    echo "iTerm Settings loaded from dotfiles repo"
 fi
 
 echo "Done"
