40a2b07ce7cf72c1900c38491724c6e762a15d61
Jitin Nair <2485715+jitinnair1@users.noreply.github.com>
2026-07-06 22:37:44 +0530

    added opencode

--- unstow.sh
+++ unstow.sh
@@ -6,7 +6,7 @@ TARGET_DIR="$HOME"
 cd "$DOTFILES_DIR"
 
 echo "Removing stow-managed symlinks"
-stow --target="$TARGET_DIR" --delete --verbose shell nvim tmux alacritty ghostty zed others i3 vim
+stow --target="$TARGET_DIR" --delete --verbose shell nvim tmux alacritty ghostty zed opencode others i3 vim
 
 echo "Done"
 
